974b4541b3e513fbbddf539ab73031b1ef84c627
MacretEtan <etan.macret@3wa.com>
2025-02-23 18:17:40 +0100

    remove public

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Portfolio\Router\Router;
+
+require 'lib/autoload.php';
+
+new Router();
--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Portfolio\Router\Router;
-
-require dirname(__DIR__) . '/lib/autoload.php';
-
-new Router();
